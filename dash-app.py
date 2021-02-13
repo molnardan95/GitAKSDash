15fa236cab76eb0614c077c3a5894c62c1e17cf6
molnardan95 <molnardan95@gmail.com>
2021-02-13 17:05:59 +0000

    Port to 8080 in py file and Docker

--- dash-app.py
+++ dash-app.py
@@ -73,7 +73,4 @@ def output(n_clicks, number):
 
 
 if __name__ == "__main__":
-    # Get port and debug mode from environment variables
-    port = os.environ.get('dash_port')
-    debug = os.environ.get('dash_debug')=="True"
-    app.run_server(debug=debug, host="0.0.0.0", port=port)
+    app.run_server(debug=True, host="0.0.0.0", port=8080)
