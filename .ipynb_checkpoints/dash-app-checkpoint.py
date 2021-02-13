da68a5c560f723aa1bdb8b00ffd19a40c174bf7c
molnardan95 <molnardan95@gmail.com>
2021-02-13 16:45:21 +0000

    Initial Commit

---
+++ .ipynb_checkpoints/dash-app-checkpoint.py
@@ -0,0 +1,79 @@
+import os
+import dash
+import dash_core_components as dcc 
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import plotly.graph_objs as go
+import numpy as np
+
+app = dash.Dash(__name__)
+
+# +
+# Creating DATA
+np.random.seed(42)
+random_x = np.random.randint(1,101,100)
+random_y = np.random.randint(1,101,100)
+
+app.layout = html.Div([
+    dcc.Input(
+        id='number-in',
+        value=1,
+        style={'fontSize':28}
+    ),
+    html.Button(
+        id='submit-button',
+        n_clicks=0,
+        children='Submit',
+        style={'fontSize':28}
+    ),
+    html.H1(id='number-out'),
+    dcc.Graph(id='scatterplot',
+                    figure = {'data':[
+                            go.Scatter(
+                            x=random_x,
+                            y=random_y,
+                            mode='markers',
+                            marker = {
+                                'size':12,
+                                'color': 'rgb(51,204,153)',
+                                'symbol':'pentagon',
+                                'line':{'width':2}
+                            }
+                            )],
+                    'layout':go.Layout(title='My Scatterplot',
+                                        xaxis = {'title':'Some X title'})}
+                    ),
+                    dcc.Graph(id='scatterplot2',
+                                        figure = {'data':[
+                                                go.Scatter(
+                                                x=random_x,
+                                                y=random_y,
+                                                mode='markers',
+                                                marker = {
+                                                    'size':12,
+                                                    'color': 'rgb(200,204,53)',
+                                                    'symbol':'pentagon',
+                                                    'line':{'width':2}
+                                                }
+                                                )],
+                                        'layout':go.Layout(title='Second Plot',
+                                                            xaxis = {'title':'Some X title'})}
+                                        )
+])
+
+
+# -
+
+@app.callback(
+    Output('number-out', 'children'),
+    [Input('submit-button', 'n_clicks')],
+    [State('number-in', 'value')])
+def output(n_clicks, number):
+    return '{} displayed after {} clicks'.format(number,n_clicks)
+
+
+if __name__ == "__main__":
+    # Get port and debug mode from environment variables
+    port = os.environ.get('dash_port')
+    debug = os.environ.get('dash_debug')=="True"
+    app.run_server(debug=debug, host="0.0.0.0", port=port)
