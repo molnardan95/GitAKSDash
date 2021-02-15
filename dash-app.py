585bef792b61239d67d970ad3af199b18b9feae0
molnardan95 <molnardan95@gmail.com>
2021-02-15 09:48:41 +0000

    Target Port added and dash-app file port added

--- dash-app.py
+++ dash-app.py
@@ -1,77 +1,40 @@
-import os
-import dash
-import dash_core_components as dcc 
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
-import plotly.graph_objs as go
-import numpy as np
-
-app = dash.Dash(__name__)
-server = app.server
-
 # +
-# Creating DATA
-np.random.seed(42)
-random_x = np.random.randint(1,101,100)
-random_y = np.random.randint(1,101,100)
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
 
-app.layout = html.Div([
-    dcc.Input(
-        id='number-in',
-        value=1,
-        style={'fontSize':28}
-    ),
-    html.Button(
-        id='submit-button',
-        n_clicks=0,
-        children='Submit',
-        style={'fontSize':28}
-    ),
-    html.H1(id='number-out'),
-    dcc.Graph(id='scatterplot',
-                    figure = {'data':[
-                            go.Scatter(
-                            x=random_x,
-                            y=random_y,
-                            mode='markers',
-                            marker = {
-                                'size':12,
-                                'color': 'rgb(51,204,153)',
-                                'symbol':'pentagon',
-                                'line':{'width':2}
-                            }
-                            )],
-                    'layout':go.Layout(title='My Scatterplot',
-                                        xaxis = {'title':'Some X title'})}
-                    ),
-                    dcc.Graph(id='scatterplot2',
-                                        figure = {'data':[
-                                                go.Scatter(
-                                                x=random_x,
-                                                y=random_y,
-                                                mode='markers',
-                                                marker = {
-                                                    'size':12,
-                                                    'color': 'rgb(200,204,53)',
-                                                    'symbol':'pentagon',
-                                                    'line':{'width':2}
-                                                }
-                                                )],
-                                        'layout':go.Layout(title='Second Plot',
-                                                            xaxis = {'title':'Some X title'})}
-                                        )
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
 ])
 
 
+if __name__ == "__main__":
+    app.run_server(port = 80)
 # -
 
-@app.callback(
-    Output('number-out', 'children'),
-    [Input('submit-button', 'n_clicks')],
-    [State('number-in', 'value')])
-def output(n_clicks, number):
-    return '{} displayed after {} clicks'.format(number,n_clicks)
 
-
-if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=int("8080"), debug=False)
