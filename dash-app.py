5355caeaa2e31ba90df3cc6ea30ff36e309b5b6e
molnardan95 <molnardan95@gmail.com>
2021-02-16 09:41:55 +0000

    Test Change to App

--- dash-app.py
+++ dash-app.py
@@ -7,7 +7,8 @@ import dash_html_components as html
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1("Hello Dash")
+    html.H1("Hello Dash"),
+    html.H2("This is working")
 ])
 
 if __name__ == "__main__":
